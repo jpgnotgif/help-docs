d6c07f308087a1e84da0dfe1d5c9fb03eb9aa1cd
Joe Gutierrez <jgutierrez@recurly.com>
2017-12-05 15:24:27 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # help-docs
+This repo houses a slack bot that replies with Recurly documentation urls when provided a topic
+
+## Bootstrapping
+1. Clone this repo
+2. Get ngrok up and running for local development by following this
+   tutorial:
+https://api.slack.com/tutorials/tunneling-with-ngrok
+
+3. Edit the `Request URL` field on the slash command page in the app via
+   https://api.slack.com/apps/A8A99D6NR/slash-commands?. The url should
+be the one labeled under "Forwarding" when you started ngrok
+
+4. Run `npm install`
+5. Create a file to house the slack client id
+6. Create a file to house the slack secret id
+7. Start the server like so:
+`SLACK_CLIENT_ID=<app_client_id> SLACK_SECRET_ID=<app_secret_id> npm
+start`
---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const topics = JSON.parse(fs.readFileSync('./topics.json', 'utf8'));
+
+const clientId     = '282209670404.282315448773';
+const clientSecret = 'eb093a3e2468d065cf9f6a9b0c0474bc';
+
+const app = express();
+const port = 4390;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(port, function () {
+  console.log(`[help_docs] Listening on port ${port}`);
+  console.log("[help_docs] Loaded topics %o", topics);
+});
+
+app.get('/', function(req, res) {
+  res.send('Ngrok is working! Path Hit: ' + req.url);
+});
+
+app.get('/oauth', function(req, res) {
+  if (!req.query.code) {
+    res.status(500);
+    res.send({"Error": "Looks like we're not getting code."});
+    console.log("Looks like we're not getting code.");
+  } else {
+    // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+    request({
+        url: 'https://slack.com/api/oauth.access', //URL to hit
+        qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+        method: 'GET', //Specify the method
+      }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+            }
+        })
+    }
+});
+
+// Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
+app.post('/topic', (req, res) => {
+  const topicName = req.body.text;
+  const url = topics[topicName];
+
+  if (url !== undefined) {
+    res.send(`[help_docs] ${url}`);
+  } else {
+    res.send(`[help_docs] :thinking_face: could not find page for topic "${topicName}" :thinking_face:`);
+  }
+});
